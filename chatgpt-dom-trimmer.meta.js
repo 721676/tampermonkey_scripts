f77574d5ad6d10acd9126f6809fc7e490e8ec8db
Nektosaw <36419050+721676@users.noreply.github.com>
2026-03-12 13:45:24 +0800

    Update chatgpt-dom-trimmer.meta.js

--- chatgpt-dom-trimmer.meta.js
+++ chatgpt-dom-trimmer.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ChatGPT DOM Trimmer
-// @namespace    https://github.com/MYLOGIN/tampermonkey_scripts
+// @namespace    https://github.com/721676/tampermonkey_scripts
 // @version      1.1.0
 // @description  Авто-обрезка старых сообщений в чате ChatGPT + перемещаемая настраиваемая панель
 // @author       you
@@ -10,10 +10,10 @@
 // @exclude      https://chatgpt.com/codex*
 // @exclude      https://chat.openai.com/gpts/editor*
 // @exclude      https://chat.openai.com/codex*
-// @downloadURL  https://raw.githubusercontent.com/MYLOGIN/tampermonkey_scripts/main/chatgpt-dom-trimmer.user.js
-// @updateURL    https://raw.githubusercontent.com/MYLOGIN/tampermonkey_scripts/main/chatgpt-dom-trimmer.meta.js
-// @homepageURL  https://github.com/MYLOGIN/tampermonkey_scripts
-// @supportURL   https://github.com/MYLOGIN/tampermonkey_scripts/issues
+// @downloadURL  https://raw.githubusercontent.com/721676/tampermonkey_scripts/main/chatgpt-dom-trimmer.user.js
+// @updateURL    https://raw.githubusercontent.com/721676/tampermonkey_scripts/main/chatgpt-dom-trimmer.meta.js
+// @homepageURL  https://github.com/721676/tampermonkey_scripts
+// @supportURL   https://github.com/721676/tampermonkey_scripts/issues
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
